f0d2674e372497f53b96b1165c3520320c5dc9aa
Rashed Miya <rashedmiyaofficial@gmail.com>
2025-12-25 22:01:32 +0600

    add

--- OOPS/class.php
+++ OOPS/class.php
@@ -1,8 +1,12 @@
 <?php 
-
-    class Student {
+ 
+     class Student {
         public $id = 10;
-    }
+        public $name = "Akash";
+     }
 
-    $student = new Student();
-    echo $student->id;
+     $student = new Student();
+     echo $student->name;
+     $student->name = "Rahul";
+     echo "<br>";
+     echo $student->name;  
